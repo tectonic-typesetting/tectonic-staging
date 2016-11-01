530987c0e802f09d940c9cda28807eba89d62359
Peter Williams <peter@newton.cx>
2016-11-01 14:31:28 -0400

    tidy_kpathsea/c-auto.h: merge into config.h

--- tidy_kpathsea/config.h
+++ tidy_kpathsea/config.h
@@ -22,8 +22,37 @@
 
 #define DEV_NULL "/dev/null"
 
-/* System dependencies that are figured out by `configure'.  */
-#include <kpathsea/c-auto.h>
+#define HAVE_ASSERT_H 1
+#define HAVE_DECL_ISASCII 1
+#define HAVE_DECL_PUTENV 1
+#define HAVE_DIRENT_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_FLOAT_H 1
+#define HAVE_GETCWD 1
+#define HAVE_GETWD 1
+#define HAVE_LIMITS_H 1
+#define HAVE_MEMCMP 1
+#define HAVE_MEMCPY 1
+#define HAVE_MEMORY_H 1
+#define HAVE_PWD_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRCHR 1
+#define HAVE_STRING_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRRCHR 1
+#define HAVE_SYS_PARAM_H 1
+#define HAVE_UNISTD_H 1
+
+#define KPSEVERSION "kpathsea version 6.2.3/dev"
+#define MAKE_TEX_FMT_BY_DEFAULT 0
+#define MAKE_TEX_MF_BY_DEFAULT 0
+#define MAKE_TEX_PK_BY_DEFAULT 0
+#define MAKE_TEX_TEX_BY_DEFAULT 0
+#define MAKE_TEX_TFM_BY_DEFAULT 0
+#define SIZEOF_LONG 8
+#define STDC_HEADERS 1
+#define _DARWIN_USE_64_BIT_INODE 1
 
 /* Some drivers have partially integrated kpathsea changes.  */
 #ifndef KPATHSEA

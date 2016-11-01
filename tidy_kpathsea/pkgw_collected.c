56d39f2e495cc38a8b8c827a255963de781b62e0
Peter Williams <peter@newton.cx>
2016-11-01 14:22:10 -0400

    tidy_kpathsea: start collecting tiny files here

--- tidy_kpathsea/pkgw_collected.c
+++ tidy_kpathsea/pkgw_collected.c
@@ -1,4 +1,4 @@
-/* absolute.c: test if a filename is absolute or explicitly relative.
+/* Collected kpathsea files in the tidied workalike version.
 
    Copyright 1993, 1994, 1995, 2008, 2009, 2010, 2011 Karl Berry.
    Copyright 1997, 2002, 2005 Olaf Weber.
@@ -17,7 +17,6 @@
    along with this library; if not, see <http://www.gnu.org/licenses/>.  */
 
 #include <kpathsea/config.h>
-
 #include <kpathsea/absolute.h>
 #include <kpathsea/c-pathch.h>
 
@@ -27,37 +26,12 @@
 boolean
 kpathsea_absolute_p (kpathsea kpse, const_string filename, boolean relative_ok)
 {
-#ifdef VMS
-#include <string.h>
-  (void)kpse; /* currenty not used */
-  return strcspn (filename, "]>:") != strlen (filename);
-#else /* not VMS */
   boolean absolute;
   boolean explicit_relative;
 
-  absolute = IS_DIR_SEP (*filename)
-#ifdef DOSISH
-                     /* Novell allows non-alphanumeric drive letters. */
-                     || (*filename && IS_DEVICE_SEP (filename[1]))
-#endif /* DOSISH */
-#ifdef WIN32
-                     /* UNC names */
-                     || (*filename == '\\' && filename[1] == '\\')
-                     || (*filename == '/' && filename[1] == '/')
-#endif /* WIN32 */
-#ifdef AMIGA
-                     /* Colon anywhere means a device.  */
-                     || strchr (filename, ':')
-#endif /* AMIGA */
-                      ;
+  absolute = IS_DIR_SEP (*filename);
   explicit_relative
     = relative_ok
-#ifdef AMIGA
-      /* Leading / is like `../' on Unix and DOS.  Allow Unix syntax,
-         too, though, because of possible patch programs like
-         `UnixDirsII' by Martin Scott.  */
-      && IS_DIR_SEP (*filename) || 0
-#endif /* AMIGA */
       && (*filename == '.' && (IS_DIR_SEP (filename[1])
                          || (filename[1] == '.' && IS_DIR_SEP (filename[2]))));
 
@@ -65,32 +39,10 @@ kpathsea_absolute_p (kpathsea kpse, const_string filename, boolean relative_ok)
   /* FIXME: On UNIX an IS_DIR_SEP of any but the last character in the name
      implies relative.  */
   return absolute || explicit_relative;
-#endif /* not VMS */
 }
 
-#if defined (KPSE_COMPAT_API)
 boolean
 kpse_absolute_p (const_string filename, boolean relative_ok)
 {
     return kpathsea_absolute_p (kpse_def, filename, relative_ok);
 }
-#endif
-
-#ifdef TEST
-int main()
-{
-  char **name;
-  char *t[] = { "./foo", "\\\\server\\foo\\bar", "ftp://localhost/foo" };
-
-  for (name = t; name - t < sizeof(t)/sizeof(char*); name++) {
-    printf ("Path `%s' %s absolute.\n", *name,
-            kpse_absolute_p(*name, true) ? "is" : "is not");
-  }
-}
-#endif /* TEST */
-
-/*
-Local variables:
-standalone-compile-command: "gcc -g -I. -I.. -DTEST absolute.c kpathsea.a"
-End:
-*/
--- tidy_kpathsea/pkgw_collected.h
+++ tidy_kpathsea/pkgw_collected.h
@@ -1,4 +1,4 @@
-/* absolute.h: declare absolute filename predicate.
+/* Collected kpathsea files in the tidied workalike version.
 
    Copyright 1993, 1994, 2008, 2009 Karl Berry.
    Copyright 1999, 2005 Olaf Weber.
@@ -16,8 +16,8 @@
    You should have received a copy of the GNU Lesser General Public License
    along with this library; if not, see <http://www.gnu.org/licenses/>.  */
 
-#ifndef KPATHSEA_ABSOLUTE_H
-#define KPATHSEA_ABSOLUTE_H
+#ifndef TIDY_KPATHSEA_COLLECTED_H
+#define TIDY_KPATHSEA_COLLECTED_H
 
 #include <kpathsea/types.h>
 #include <kpathsea/c-proto.h>
@@ -26,19 +26,11 @@
 extern "C" {
 #endif
 
-/* True if FILENAME is absolute (/foo) or, if RELATIVE_OK is true,
-   explicitly relative (./foo, ../foo), else false (foo).  */
-
-extern KPSEDLL boolean kpathsea_absolute_p (kpathsea kpse,
-                                   const_string filename, boolean relative_ok);
-
-#if defined(KPSE_COMPAT_API)
-extern KPSEDLL boolean kpse_absolute_p (const_string filename,
-                                        boolean relative_ok);
-#endif
+extern KPSEDLL boolean kpathsea_absolute_p (kpathsea kpse, const_string filename, boolean relative_ok);
+extern KPSEDLL boolean kpse_absolute_p (const_string filename, boolean relative_ok);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* not KPATHSEA_ABSOLUTE_H */
+#endif /* not TIDY_KPATHSEA_COLLECTED_H */

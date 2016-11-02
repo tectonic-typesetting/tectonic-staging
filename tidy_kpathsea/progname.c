ec78e1ca92d447b99b97210761c8105842a57dc1
Peter Williams <peter@newton.cx>
2016-11-02 19:20:03 -0400

    tidy_kpathsea: tidy c-pathmx.h into the void

--- tidy_kpathsea/progname.c
+++ tidy_kpathsea/progname.c
@@ -18,7 +18,6 @@
 
 #include <tidy_kpathsea/config.h>
 #include <tidy_kpathsea/pkgw_collected.h>
-#include <tidy_kpathsea/c-pathmx.h>
 #include <tidy_kpathsea/c-stat.h>
 #include <tidy_kpathsea/pathsearch.h>
 /* For kpse_reset_progname */

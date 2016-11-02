bfc14806f5ae93f627e02aa9e6dfea4e94c023c5
Peter Williams <peter@newton.cx>
2016-11-02 19:09:41 -0400

    tidy_kpathsea/c-std.h: tidy

--- tidy_kpathsea/c-std.h
+++ tidy_kpathsea/c-std.h
@@ -22,55 +22,15 @@
 /* Header files that essentially all of our sources need, and
    that all implementations have.  We include these first, to help with
    NULL being defined multiple times.  */
-/* Workaround against a <math.h> MSVC bug : it can't be declared
-   extern "C" in a c++ source file. */
-#ifndef __cplusplus
+
 #include <math.h>
-/* apparently M_PI isn't defined by <math.h> under older VC */
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
-#endif
 #include <stdio.h>
 #include <stdarg.h>
 
 /* Be sure we have constants from <unistd.h>.  */
 #include <tidy_kpathsea/c-unistd.h>
 
-#ifdef HAVE_STDLIB_H
 #include <stdlib.h>
-/* Include <stdlib.h> before <stddef.h>, to help avoid NULL
-   redefinitions on some systems.  (We don't include <stddef.h>
-   ourselves any more, but FYI.)  */
-#else
-/* It's impossible to say for sure what the system will deign to put in
-   <stdlib.h>, but let's hope it's at least this.  */
-extern char *getenv ();
-#endif /* not HAVE_STDLIB_H */
-
-#ifdef WIN32
-#include <malloc.h>
-#else
-#ifndef STDC_HEADERS
-#ifndef ALLOC_RETURN_TYPE
-#define ALLOC_RETURN_TYPE void
-#endif /* not ALLOC_RETURN_TYPE */
-extern ALLOC_RETURN_TYPE *calloc (), *malloc (), *realloc ();
-#endif /* not STDC_HEADERS */
-#endif /* not WIN32 */
-
-/* SunOS 4.1.1 gets STDC_HEADERS defined, but it doesn't provide
-   EXIT_FAILURE.  So far no system has defined one of EXIT_FAILURE and
-   EXIT_SUCCESS without the other.  */
-#ifndef EXIT_SUCCESS
-#ifdef VMS
-#define EXIT_SUCCESS 1
-#define EXIT_FAILURE 0
-#else
-#define EXIT_SUCCESS 0
-#define EXIT_FAILURE 1
-#endif
-#endif /* not EXIT_SUCCESS */
 
 /* c-memstr.h */
 
@@ -81,15 +41,6 @@ extern ALLOC_RETURN_TYPE *calloc (), *malloc (), *realloc ();
 #include <limits.h>
 
 /* Assertions are too useful to not make generally available.  */
-#ifdef HAVE_ASSERT_H
 #include <assert.h>
-#else
-#define assert(expr) /* as nothing */
-#endif
-
-#ifdef VMS
-#include <unixlib.h>
-#include <unixio.h>
-#endif /* not VMS */
 
 #endif /* not KPATHSEA_C_STD_H */

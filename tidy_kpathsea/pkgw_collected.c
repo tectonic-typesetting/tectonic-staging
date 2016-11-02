9c9eafbc55d630df9509598a9ef06e8340f658b2
Peter Williams <peter@newton.cx>
2016-11-01 23:53:03 -0400

    tidy_kpathsea: fold in fn

--- tidy_kpathsea/pkgw_collected.c
+++ tidy_kpathsea/pkgw_collected.c
@@ -24,6 +24,7 @@
 #include <tidy_kpathsea/concatn.h>
 #include <tidy_kpathsea/db.h>
 #include <tidy_kpathsea/debug.h>
+#include <tidy_kpathsea/fn.h>
 #include <tidy_kpathsea/hash.h>
 #include <tidy_kpathsea/line.h>
 #include <tidy_kpathsea/paths.h>
@@ -517,4 +518,105 @@ find_suffix (const_string name)
   }
 
   return dot_pos + 1;
+}
+
+/* fn.c */
+
+
+
+/* /usr/local/lib/texmf/fonts/public/cm/pk/ljfour/cmr10.300pk is 58
+   chars, so ASCII `K' seems a good choice. */
+#define CHUNK_SIZE 75
+
+
+fn_type
+fn_init (void)
+{
+  fn_type ret;
+
+  FN_ALLOCATED (ret) = FN_LENGTH (ret) = 0;
+  FN_STRING (ret) = NULL;
+
+  return ret;
+}
+
+
+fn_type
+fn_copy0 (const_string s,  unsigned len)
+{
+  fn_type ret;
+
+  FN_ALLOCATED (ret) = CHUNK_SIZE > len ? CHUNK_SIZE : len + 1;
+  FN_STRING (ret) = (string)xmalloc (FN_ALLOCATED (ret));
+
+  strncpy (FN_STRING (ret), s, len);
+  FN_STRING (ret)[len] = 0;
+  FN_LENGTH (ret) = len + 1;
+
+  return ret;
+}
+
+/* Don't think we ever try to free something that might usefully be
+   empty, so give fatal error if nothing allocated.  */
+
+void
+fn_free (fn_type *f)
+{
+  assert (FN_STRING (*f) != NULL);
+  free (FN_STRING (*f));
+  FN_STRING (*f) = NULL;
+  FN_ALLOCATED (*f) = 0;
+  FN_LENGTH (*f) = 0;
+}
+
+/* An arithmetic increase seems more reasonable than geometric.  We
+   don't increase the length member since it may be more convenient for
+   the caller to add than subtract when appending the stuff that will
+   presumably follow.  */
+
+static void
+grow (fn_type *f,  unsigned len)
+{
+  while (FN_LENGTH (*f) + len > FN_ALLOCATED (*f))
+    {
+      FN_ALLOCATED (*f) += CHUNK_SIZE;
+      XRETALLOC (FN_STRING (*f), FN_ALLOCATED (*f), char);
+    }
+}
+
+
+void
+fn_1grow (fn_type *f,  char c)
+{
+  grow (f, 1);
+  FN_STRING (*f)[FN_LENGTH (*f)] = c;
+  FN_LENGTH (*f)++;
+}
+
+
+void
+fn_grow (fn_type *f,  const_string source,  unsigned len)
+{
+  grow (f, len);
+  strncpy (FN_STRING (*f) + FN_LENGTH (*f), source, len);
+  FN_LENGTH (*f) += len;
+}
+
+
+void
+fn_str_grow (fn_type *f,  const_string s)
+{
+  unsigned more_len = strlen (s);
+  grow (f, more_len);
+  strcat (FN_STRING (*f), s);
+  FN_LENGTH (*f) += more_len;
+}
+
+
+void
+fn_shrink_to (fn_type *f,  unsigned loc)
+{
+  assert (FN_LENGTH (*f) > loc);
+  FN_STRING (*f)[loc] = 0;
+  FN_LENGTH (*f) = loc + 1;
 }

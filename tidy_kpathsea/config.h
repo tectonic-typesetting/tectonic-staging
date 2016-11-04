8546a803157eecfd6a987cc3e99330c962916117
Peter Williams <peter@newton.cx>
2016-11-03 22:32:11 -0400

    tidy_kpathsea: fold in lib.h

--- tidy_kpathsea/config.h
+++ tidy_kpathsea/config.h
@@ -127,11 +127,106 @@ extern KPSEDLL FILE *fopen (const char *filename, const char *mode);
 #define fclose kpse_fclose_trace
 extern KPSEDLL int fclose (FILE *);
 
+/* lib.h */
+
+#define FATAL_PERROR(str) do {			       \
+  fprintf (stderr, "%s: ", kpse_def->invocation_name); \
+  perror (str); exit (EXIT_FAILURE); } while (0)
+#define START_FATAL() do { \
+  fprintf (stderr, "%s: fatal: ", kpse_def->invocation_name);
+#define END_FATAL() fputs (".\n", stderr); exit (1); } while (0)
+
+#define FATAL(str)                                                      \
+  START_FATAL (); fputs (str, stderr); END_FATAL ()
+#define FATAL1(str, e1)                                                 \
+  START_FATAL (); fprintf (stderr, str, e1); END_FATAL ()
+#define FATAL2(str, e1, e2)                             \
+   START_FATAL (); fprintf (stderr, str, e1, e2); END_FATAL ()
+#define FATAL3(str, e1, e2, e3)                             \
+   START_FATAL (); fprintf (stderr, str, e1, e2, e3); END_FATAL ()
+#define FATAL4(str, e1, e2, e3, e4)                             \
+   START_FATAL (); fprintf (stderr, str, e1, e2, e3, e4); END_FATAL ()
+#define FATAL5(str, e1, e2, e3, e4, e5)                             \
+   START_FATAL (); fprintf (stderr, str, e1, e2, e3, e4, e5); END_FATAL ()
+#define FATAL6(str, e1, e2, e3, e4, e5, e6)                       \
+   START_FATAL (); fprintf (stderr, str, e1, e2, e3, e4, e5, e6); END_FATAL ()
+
+#define START_WARNING() do { fputs ("warning: ", stderr)
+#define END_WARNING() fputs (".\n", stderr); fflush (stderr); } while (0)
+
+#define WARNING(str)                                                    \
+  START_WARNING (); fputs (str, stderr); END_WARNING ()
+#define WARNING1(str, e1)                                               \
+  START_WARNING (); fprintf (stderr, str, e1); END_WARNING ()
+#define WARNING2(str, e1, e2)                                           \
+  START_WARNING (); fprintf (stderr, str, e1, e2); END_WARNING ()
+#define WARNING3(str, e1, e2, e3)                                       \
+  START_WARNING (); fprintf (stderr, str, e1, e2, e3); END_WARNING ()
+#define WARNING4(str, e1, e2, e3, e4)                                   \
+  START_WARNING (); fprintf (stderr, str, e1, e2, e3, e4); END_WARNING ()
+
+#define LIB_START_FATAL() do { \
+  fprintf (stderr, "%s: fatal: ", kpse->invocation_name);
+
+#define LIB_FATAL(str)                                                  \
+  LIB_START_FATAL (); fputs (str, stderr); END_FATAL ()
+#define LIB_FATAL1(str, e1)                                             \
+  LIB_START_FATAL (); fprintf (stderr, str, e1); END_FATAL ()
+#define LIB_FATAL2(str, e1, e2)                                         \
+  LIB_START_FATAL (); fprintf (stderr, str, e1, e2); END_FATAL ()
+
+#define STREQ(s1, s2) (((s1) != NULL) && ((s2) != NULL) && (strcmp (s1, s2) == 0))
+#define STRNEQ(s1, s2, n) ((s1) && (s2) && (strncmp (s1, s2, n) == 0))
+
+#define FILESTRCASEEQ STREQ
+#define FILESTRNCASEEQ STRNEQ
+#define FILECHARCASEEQ(c1,c2) ((c1) == (c2))
+
+#define MAX_INT_LENGTH 21
+
+#define ENVVAR(test, default) (getenv (test) ? (test) : (default))
+
+extern KPSEDLL string concat (const_string s1, const_string s2);
+extern KPSEDLL string concat3 (const_string, const_string, const_string);
+extern KPSEDLL string xstrdup (const_string s);
+extern KPSEDLL string uppercasify (const_string s);
+extern KPSEDLL unsigned atou (const_string);
+extern KPSEDLL boolean same_file_p (const_string filename1, const_string filename2);
+extern KPSEDLL const_string xbasename (const_string name);
+extern KPSEDLL string xdirname (const_string name);
+extern KPSEDLL const_string find_suffix (const_string name);
+extern KPSEDLL string remove_suffix (const_string name);
+extern KPSEDLL string make_suffix (const_string s,  const_string suffix);
+extern KPSEDLL string make_prefix (string stem_prefix, string name);
+extern KPSEDLL const_string extend_filename (const_string name, const_string suffix);
+extern KPSEDLL void kpathsea_xputenv (kpathsea kpse, const_string var, const_string value);
+extern KPSEDLL void kpathsea_xputenv_int (kpathsea kpse, const_string var, int value);
+extern KPSEDLL void xputenv (const_string var, const_string value);
+extern KPSEDLL void xputenv_int (const_string var, int value);
+extern KPSEDLL string xgetcwd (void);
+extern KPSEDLL boolean kpathsea_dir_p (kpathsea kpse, string fn);
+extern KPSEDLL boolean dir_p (string fn);
+extern KPSEDLL int dir_links (const_string fn, long nlinks);
+extern KPSEDLL int kpathsea_dir_links (kpathsea kpse, const_string fn, long nlinks);
+extern KPSEDLL FILE *xfopen (const_string filename, const_string mode);
+extern KPSEDLL void xfclose (FILE *fp, const_string filename);
+extern KPSEDLL void xfseek (FILE *fp, long offset, int wherefrom, const_string filename);
+extern KPSEDLL void xfseeko (FILE *fp, off_t offset, int wherefrom, const_string filename);
+extern KPSEDLL long xftell (FILE *fp, const_string filename);
+extern KPSEDLL off_t xftello (FILE *fp, const_string filename);
+extern KPSEDLL address xmalloc (size_t size);
+extern KPSEDLL address xrealloc (address old_address, size_t new_size);
+extern KPSEDLL address xcalloc (size_t nelem, size_t elsize);
+
+/* (Re)Allocate N items of type T using xmalloc/xrealloc.  */
+#define XTALLOC(n, t) ((t *) xmalloc ((n) * sizeof (t)))
+#define XTALLOC1(t) XTALLOC (1, t)
+#define XRETALLOC(addr, n, t) ((addr) = (t *) xrealloc (addr, (n) * sizeof(t)))
+
 #ifdef __cplusplus
 }
 #endif
 
-#include <tidy_kpathsea/lib.h>      /* STREQ, etc. */
 #include <tidy_kpathsea/types.h>    /* <sys/types.h>, boolean, string, etc. */
 #include <tidy_kpathsea/progname.h> /* for kpse_invocation_*name */
 

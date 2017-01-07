c1b764d180050991070201a74072bc54bc186b33
Peter Williams <peter@newton.cx>
2017-01-07 14:20:29 -0600

    zip2tarindex/src/builder.rs: work on adding indexing

--- zip2tarindex/src/builder.rs
+++ zip2tarindex/src/builder.rs
@@ -11,13 +11,13 @@ use header::{bytes2path, path2bytes};
 ///
 /// This structure has methods for building up an archive from scratch into any
 /// arbitrary writer.
-pub struct HackedBuilder<WO: Write, WI: Write> {
+pub struct HackedBuilder<WO: Write + Seek, WI: Write> {
     finished: bool,
     obj: Option<WO>,
     index: Option<WI>,
 }
 
-impl<WO: Write, WI: Write> HackedBuilder<WO, WI> {
+impl<WO: Write + Seek, WI: Write> HackedBuilder<WO, WI> {
     /// Create a new archive builder with the underlying object as the
     /// destination of all data written.
     pub fn new(obj: WO, index: WI) -> HackedBuilder<WO, WI> {
@@ -32,16 +32,20 @@ impl<WO: Write, WI: Write> HackedBuilder<WO, WI> {
         self.obj.as_mut().unwrap()
     }
 
+    fn inner_index(&mut self) -> &mut WI {
+        self.index.as_mut().unwrap()
+    }
+
     /// Unwrap this archive, returning the underlying object.
     ///
     /// This function will finish writing the archive if the `finish` function
     /// hasn't yet been called, returning any I/O error which happens during
     /// that operation.
-    pub fn into_inner(mut self) -> io::Result<WO> {
+    pub fn into_inner(mut self) -> io::Result<(WO, WI)> {
         if !self.finished {
             try!(self.finish());
         }
-        Ok(self.obj.take().unwrap())
+        Ok((self.obj.take().unwrap(), self.index.take().unwrap()))
     }
 
     /// Adds a new entry to this archive.
@@ -82,97 +86,23 @@ impl<WO: Write, WI: Write> HackedBuilder<WO, WI> {
     /// ```
     pub fn append<R: Read>(&mut self, header: &Header, mut data: R)
                            -> io::Result<()> {
-        append(self.inner(), header, &mut data)
-    }
+        // tar-rs implements this method by handing off to a standalone
+        // function, but its style doesn't work for us since we can't get away
+        // with just trait objects. (It's not clear to me why tar-rs does
+        // things the way it does.) We just copy the implementation here.
+        let mut dst = self.obj.as_mut().unwrap();
 
-    /// Adds a file on the local filesystem to this archive.
-    ///
-    /// This function will open the file specified by `path` and insert the file
-    /// into the archive with the appropriate metadata set, returning any I/O
-    /// error which occurs while writing. The path name for the file inside of
-    /// this archive will be the same as `path`, and it is required that the
-    /// path is a relative path.
-    ///
-    /// Note that this will not attempt to seek the archive to a valid position,
-    /// so if the archive is in the middle of a read or some other similar
-    /// operation then this may corrupt the archive.
-    ///
-    /// Also note that after all files have been written to an archive the
-    /// `finish` function needs to be called to finish writing the archive.
-    ///
-    /// # Examples
-    ///
-    /// ```no_run
-    /// use tar::HackedBuilder;
-    ///
-    /// let mut ar = HackedBuilder::new(Vec::new());
-    ///
-    /// ar.append_path("foo/bar.txt").unwrap();
-    /// ```
-    pub fn append_path<P: AsRef<Path>>(&mut self, path: P) -> io::Result<()> {
-        append_path(self.inner(), path.as_ref())
-    }
+        try!(dst.write_all(header.as_bytes()));
+        let len = try!(io::copy(&mut data, &mut dst));
 
-    /// Adds a file to this archive with the given path as the name of the file
-    /// in the archive.
-    ///
-    /// This will use the metadata of `file` to populate a `Header`, and it will
-    /// then append the file to the archive with the name `path`.
-    ///
-    /// Note that this will not attempt to seek the archive to a valid position,
-    /// so if the archive is in the middle of a read or some other similar
-    /// operation then this may corrupt the archive.
-    ///
-    /// Also note that after all files have been written to an archive the
-    /// `finish` function needs to be called to finish writing the archive.
-    ///
-    /// # Examples
-    ///
-    /// ```no_run
-    /// use std::fs::File;
-    /// use tar::HackedBuilder;
-    ///
-    /// let mut ar = HackedBuilder::new(Vec::new());
-    ///
-    /// // Open the file at one location, but insert it into the archive with a
-    /// // different name.
-    /// let mut f = File::open("foo/bar/baz.txt").unwrap();
-    /// ar.append_file("bar/baz.txt", &mut f).unwrap();
-    /// ```
-    pub fn append_file<P: AsRef<Path>>(&mut self, path: P, file: &mut fs::File)
-                                       -> io::Result<()> {
-        append_file(self.inner(), path.as_ref(), file)
-    }
+        // Pad with zeros if necessary.
+        let buf = [0; 512];
+        let remaining = 512 - (len % 512);
+        if remaining < 512 {
+            try!(dst.write_all(&buf[..remaining as usize]));
+        }
 
-    /// Adds a directory to this archive with the given path as the name of the
-    /// directory in the archive.
-    ///
-    /// This will use `stat` to populate a `Header`, and it will then append the
-    /// directory to the archive with the name `path`.
-    ///
-    /// Note that this will not attempt to seek the archive to a valid position,
-    /// so if the archive is in the middle of a read or some other similar
-    /// operation then this may corrupt the archive.
-    ///
-    /// Also note that after all files have been written to an archive the
-    /// `finish` function needs to be called to finish writing the archive.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use std::fs;
-    /// use tar::HackedBuilder;
-    ///
-    /// let mut ar = HackedBuilder::new(Vec::new());
-    ///
-    /// // Use the directory at one location, but insert it into the archive
-    /// // with a different name.
-    /// ar.append_dir("bardir", ".").unwrap();
-    /// ```
-    pub fn append_dir<P, Q>(&mut self, path: P, src_path: Q) -> io::Result<()>
-        where P: AsRef<Path>, Q: AsRef<Path>
-    {
-        append_dir(self.inner(), path.as_ref(), src_path.as_ref())
+        Ok(())
     }
 
     /// Finish writing this archive, emitting the termination sections.
@@ -191,82 +121,7 @@ impl<WO: Write, WI: Write> HackedBuilder<WO, WI> {
     }
 }
 
-fn append(mut dst: &mut Write,
-          header: &Header,
-          mut data: &mut Read) -> io::Result<()> {
-    try!(dst.write_all(header.as_bytes()));
-    let len = try!(io::copy(&mut data, &mut dst));
-
-    // Pad with zeros if necessary.
-    let buf = [0; 512];
-    let remaining = 512 - (len % 512);
-    if remaining < 512 {
-        try!(dst.write_all(&buf[..remaining as usize]));
-    }
-
-    Ok(())
-}
-
-fn append_path(dst: &mut Write, path: &Path) -> io::Result<()> {
-    let stat = try!(fs::metadata(path));
-    if stat.is_file() {
-        append_fs(dst, path, &stat, &mut try!(fs::File::open(path)))
-    } else if stat.is_dir() {
-        append_fs(dst, path, &stat, &mut io::empty())
-    } else {
-        Err(other("path has unknown file type"))
-    }
-}
-
-fn append_file(dst: &mut Write, path: &Path, file: &mut fs::File)
-                -> io::Result<()> {
-    let stat = try!(file.metadata());
-    append_fs(dst, path, &stat, file)
-}
-
-fn append_dir(dst: &mut Write, path: &Path, src_path: &Path) -> io::Result<()> {
-    let stat = try!(fs::metadata(src_path));
-    append_fs(dst, path, &stat, &mut io::empty())
-}
-
-fn append_fs(dst: &mut Write,
-             path: &Path,
-             meta: &fs::Metadata,
-             read: &mut Read) -> io::Result<()> {
-    let mut header = Header::new_gnu();
-
-    // Try to encode the path directly in the header, but if it ends up not
-    // working (e.g. it's too long) then use the GNU-specific long name
-    // extension by emitting an entry which indicates that it's the filename
-    if let Err(e) = header.set_path(path) {
-        let data = try!(path2bytes(&path));
-        let max = header.as_old().name.len();
-        if data.len() < max {
-            return Err(e)
-        }
-        let mut header2 = Header::new_gnu();
-        header2.as_gnu_mut().unwrap().name[..13].clone_from_slice(b"././@LongLink");
-        header2.set_mode(0o644);
-        header2.set_uid(0);
-        header2.set_gid(0);
-        header2.set_mtime(0);
-        header2.set_size((data.len() + 1) as u64);
-        header2.set_entry_type(EntryType::new(b'L'));
-        header2.set_cksum();
-        let mut data2 = data.chain(io::repeat(0).take(0));
-        try!(append(dst, &header2, &mut data2));
-        // Truncate the path to store in the header we're about to emit to
-        // ensure we've got something at least mentioned.
-        let path = try!(bytes2path(Cow::Borrowed(&data[..max])));
-        try!(header.set_path(&path));
-    }
-
-    header.set_metadata(meta);
-    header.set_cksum();
-    append(dst, &header, read)
-}
-
-impl<WO: Write, WI: Write> Drop for HackedBuilder<WO, WI> {
+impl<WO: Write + Seek, WI: Write> Drop for HackedBuilder<WO, WI> {
     fn drop(&mut self) {
         let _ = self.finish();
     }

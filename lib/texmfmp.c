44a03e07034808c80d5b96e63e297209425e8986
Peter Williams <peter@newton.cx>
2017-03-05 19:57:15 -0500

    Update source files to texlive SVN r43408.

--- lib/texmfmp.c
+++ lib/texmfmp.c
@@ -2218,7 +2218,7 @@ void init_start_time() {
         if (source_date_epoch) {
             errno = 0;
             epoch = strtoull(source_date_epoch, &endptr, 10);
-            if (epoch < 0 || *endptr != '\0' || errno != 0) {
+            if (*endptr != '\0' || errno != 0) {
 FATAL1 ("invalid epoch-seconds-timezone value for environment variable $SOURCE_DATE_EPOCH: %s",
                       source_date_epoch);
             }
@@ -2818,6 +2818,8 @@ gettexstring (strnumber s)
   unsigned bytesToWrite = 0;
   poolpointer len, i, j;
   string name;
+  if (strstart[s + 1 - 65536L] < strstart[s - 65536L])
+    return NULL;
   len = strstart[s + 1 - 65536L] - strstart[s - 65536L];
   name = xmalloc(len * 3 + 1); /* max UTF16->UTF8 expansion
                                   (code units, not bytes) */

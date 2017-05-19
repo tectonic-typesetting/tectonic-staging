5168b83f5c945986fabafc00498038eebaca2af9
Peter Williams <peter@newton.cx>
2017-05-19 01:51:54 -0400

    builder/make-zipfile.py: no more spaces before parentheses
    I had a genuine switch-flip and I don't lke that style any more in Python
    code. From now on, I'm doing things like everybody else, and not putting
    spaces before parentheses in function calls and the like.

--- builder/make-zipfile.py
+++ builder/make-zipfile.py
@@ -13,43 +13,43 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import argparse, hashlib, io, os.path, sys, zipfile
 
 
-ignored_base_names = set ([
+ignored_base_names = set([
     'README',
     'ls-R',
 ])
 
-ignored_tex_paths = set ([
+ignored_tex_paths = set([
     'tex/generic/tex-ini-files/pdftexconfig.tex',
     'tex/luatex/hyph-utf8/etex.src',
 ])
 
 
-def warn (fmt, *args):
-    if len (args):
+def warn(fmt, *args):
+    if len(args):
         msg = fmt % args
     else:
-        msg = str (fmt)
-    print ('warning:', msg, file=sys.stderr)
+        msg = str(fmt)
+    print('warning:', msg, file=sys.stderr)
 
 
-def die (fmt, *args):
-    if len (args):
+def die(fmt, *args):
+    if len(args):
         msg = fmt % args
     else:
-        msg = str (fmt)
-    raise SystemExit ('error: ' + msg)
+        msg = str(fmt)
+    raise SystemExit('error: ' + msg)
 
 
-def make_arg_parser ():
-    p = argparse.ArgumentParser (
+def make_arg_parser():
+    p = argparse.ArgumentParser(
         description = __doc__,
     )
-    p.add_argument (
+    p.add_argument(
         'install_dir',
         metavar = 'DIR',
         help = 'The path of the TeXLive installation directory.'
     )
-    p.add_argument (
+    p.add_argument(
         'dest_path',
         metavar = 'PATH',
         help = 'The name of the Zip file to create.'
@@ -57,33 +57,33 @@ def make_arg_parser ():
     return p
 
 
-class Bundler (object):
-    def __init__ (self, args, zip):
+class Bundler(object):
+    def __init__(self, args, zip):
         self.args = args
         self.zip = zip
         self.item_shas = {}
         self.final_hexdigest = None
 
 
-    def add_file (self, full_path):
-        base = os.path.basename (full_path)
+    def add_file(self, full_path):
+        base = os.path.basename(full_path)
 
         if base in self.item_shas:
-            warn ('"%s" duplicates a file basename; skipping', full_path)
+            warn('"%s" duplicates a file basename; skipping', full_path)
             return
 
-        with io.open (full_path, 'rb') as f:
+        with io.open(full_path, 'rb') as f:
             # zipfile has no way to stream data into the archive (lame), so we
             # read the whole file into memory.
-            contents = f.read ()
+            contents = f.read()
 
-        self.zip.writestr (base, contents)
-        s = hashlib.sha256 ()
-        s.update (contents)
-        self.item_shas[base] = s.digest ()
+        self.zip.writestr(base, contents)
+        s = hashlib.sha256()
+        s.update(contents)
+        self.item_shas[base] = s.digest()
 
 
-    def consider_file (self, full_path, tex_path, base_name):
+    def consider_file(self, full_path, tex_path, base_name):
         """Consider adding the specified TeXLive file to the installation tree. This
         is where all the nasty hairy logic will accumulate that enables us to
         come out with a nice pretty tarball in the end.
@@ -95,73 +95,73 @@ class Bundler (object):
         if tex_path in ignored_tex_paths:
             return
 
-        if base_name.endswith ('.log'):
+        if base_name.endswith('.log'):
             return
 
-        self.add_file (full_path)
+        self.add_file(full_path)
 
 
-    def _walk_onerr (self, oserror):
-        warn ('error navigating installation tree: %s', oserror)
+    def _walk_onerr(self, oserror):
+        warn('error navigating installation tree: %s', oserror)
 
 
-    def go (self):
+    def go(self):
         # Add a couple of version files from the builder.
 
-        p = os.path.join (self.args.install_dir, 'SVNREV')
-        if os.path.exists (p):
-            self.add_file (p)
+        p = os.path.join(self.args.install_dir, 'SVNREV')
+        if os.path.exists(p):
+            self.add_file(p)
         else:
-            warn ('expected but did not see the file %s', p)
+            warn('expected but did not see the file %s', p)
 
-        p = os.path.join (self.args.install_dir, 'GITHASH')
-        if os.path.exists (p):
-            self.add_file (p)
+        p = os.path.join(self.args.install_dir, 'GITHASH')
+        if os.path.exists(p):
+            self.add_file(p)
         else:
-            warn ('expected but did not see the file %s', p)
+            warn('expected but did not see the file %s', p)
 
         # Add the main tree.
 
-        p = os.path.join (self.args.install_dir, 'texmf-dist')
-        n = len (p) + 1
+        p = os.path.join(self.args.install_dir, 'texmf-dist')
+        n = len(p) + 1
 
-        for dirpath, dirnames, filenames in os.walk (p, onerror=self._walk_onerr):
+        for dirpath, dirnames, filenames in os.walk(p, onerror=self._walk_onerr):
             for fn in filenames:
-                full = os.path.join (dirpath, fn)
+                full = os.path.join(dirpath, fn)
                 tex = full[n:]
-                self.consider_file (full, tex, fn)
+                self.consider_file(full, tex, fn)
 
         # Compute a hash of it all.
 
-        s = hashlib.sha256 ()
-        s.update (str (len (self.item_shas)))
-        s.update (b'\0')
+        s = hashlib.sha256()
+        s.update(str(len(self.item_shas)))
+        s.update(b'\0')
 
-        for name in sorted (self.item_shas.iterkeys ()):
-            s.update (name)
-            s.update (b'\0')
-            s.update (self.item_shas[name])
+        for name in sorted(self.item_shas.iterkeys()):
+            s.update(name)
+            s.update(b'\0')
+            s.update(self.item_shas[name])
 
-        self.final_hexdigest = s.hexdigest ()
-        self.zip.writestr ('SHA256SUM', self.final_hexdigest)
+        self.final_hexdigest = s.hexdigest()
+        self.zip.writestr('SHA256SUM', self.final_hexdigest)
 
 
-def main (argv):
-    args = make_arg_parser ().parse_args (argv[1:])
+def main(argv):
+    args = make_arg_parser().parse_args(argv[1:])
 
     try:
-        with zipfile.ZipFile (args.dest_path, 'w', zipfile.ZIP_DEFLATED, True) as zip:
-            b = Bundler (args, zip)
-            b.go ()
-            print (b.final_hexdigest)
+        with zipfile.ZipFile(args.dest_path, 'w', zipfile.ZIP_DEFLATED, True) as zip:
+            b = Bundler(args, zip)
+            b.go()
+            print(b.final_hexdigest)
     except Exception as e:
-        e1, e2, e3 = sys.exc_info ()
+        e1, e2, e3 = sys.exc_info()
         try:
-            os.unlink (args.dest_path)
+            os.unlink(args.dest_path)
         except:
             pass
         raise e1, e2, e3
 
 
 if __name__ == '__main__':
-    sys.exit (main (sys.argv))
+    sys.exit(main(sys.argv))

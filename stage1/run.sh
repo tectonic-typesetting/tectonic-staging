f4657dbe11a0794498188b5c91e8920225c71064
Peter Williams <peter@newton.cx>
2020-10-14 14:44:24 -0400

    stage1/run.sh: chown outputs to host-machine user when done

--- stage1/run.sh
+++ stage1/run.sh
@@ -8,3 +8,4 @@ stage1dir="$(dirname $0)"
 $stage1dir/driver.sh copy-source
 $stage1dir/driver.sh setup-build
 $stage1dir/driver.sh run-build
+$stage1dir/../stage0/driver.sh builder-bash -c "chown -R $(id -u):$(id -g) /state/outputs/stage1"
